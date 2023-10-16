c8e68602e7aec7f708523a3f1268912c45d0977e
Khushi Joshi <khushi.joshi@simptel.in>
2023-10-16 12:29:02 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'content',
+    component: ContentComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'content'
+  },
+  {
+    path: '**',
+    component: ContentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
